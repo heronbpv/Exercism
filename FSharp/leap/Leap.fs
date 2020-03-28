fd84a130218d1ac3ee5e6213165c01566cd37006
Heron Peixoto <heronbpeixoto@gmail.com>
2020-03-28 00:08:12 -0300

    Found a workable solution for the leap year problem. But it is a piramid of doom...

--- FSharp/leap/Leap.fs
+++ FSharp/leap/Leap.fs
@@ -1,7 +1,13 @@
 ﻿module Leap
 
 let leapYear (year: int): bool =
-    let ``Is Divisible by 4`` = (year % 4) = 0
-    let ``Is Divisible by 100`` = (year % 100) = 0
-    let ``Is Divisible by 400`` = (year % 400) = 0
-    ``Is Divisible by 4`` && ``Is Divisible by 100`` && ``Is Divisible by 400``
+    if ((year % 4) = 0) then
+        if ((year % 100) = 0) then
+            if ((year % 400) = 0) then
+                true
+             else
+                 false
+        else
+            true
+    else
+        false
--- FSharp/leap/Scratchpad.fsx
+++ FSharp/leap/Scratchpad.fsx
@@ -5,6 +5,20 @@
     printfn "by 4?: %s by 100?:%s by 400?:%s" ``Is Divisible by 4`` ``Is Divisible by 100`` ``Is Divisible by 400``
     //``Is Divisible by 4`` && ``Is Divisible by 100`` && ``Is Divisible by 400``
 
-leapYear 1996
+let leapYear2 (year: int) =
+    if ((year % 4) = 0) then
+        if ((year % 100) = 0) then
+            if ((year % 400) = 0) then
+                true
+             else
+                 false
+        else
+            true
+    else
+        false
 
+
+leapYear 1996
+leapYear2 1996
 leapYear 1960
+leapYear2 1960
