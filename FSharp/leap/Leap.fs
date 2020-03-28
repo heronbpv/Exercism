a6dd3ab771638fb42817d89c9bb41f8c163bf9a6
Heron Peixoto <heronbpeixoto@gmail.com>
2020-03-27 23:43:11 -0300

    First try on a solution. In a very naive way. Fails for a value of 1996 and 1960. Added a scratchpad for proper exploration.

--- FSharp/leap/Leap.fs
+++ FSharp/leap/Leap.fs
@@ -1,3 +1,7 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = failwith "You need to implement this function."
+let leapYear (year: int): bool =
+    let ``Is Divisible by 4`` = (year % 4) = 0
+    let ``Is Divisible by 100`` = (year % 100) = 0
+    let ``Is Divisible by 400`` = (year % 400) = 0
+    ``Is Divisible by 4`` && ``Is Divisible by 100`` && ``Is Divisible by 400``
---
+++ FSharp/leap/Scratchpad.fsx
@@ -0,0 +1,10 @@
+﻿let leapYear (year: int) =
+    let ``Is Divisible by 4`` = sprintf "%b" ((year % 4) = 0)
+    let ``Is Divisible by 100`` = sprintf "%b" ((year % 100) = 0)
+    let ``Is Divisible by 400`` = sprintf "%b" ((year % 400) = 0)
+    printfn "by 4?: %s by 100?:%s by 400?:%s" ``Is Divisible by 4`` ``Is Divisible by 100`` ``Is Divisible by 400``
+    //``Is Divisible by 4`` && ``Is Divisible by 100`` && ``Is Divisible by 400``
+
+leapYear 1996
+
+leapYear 1960
